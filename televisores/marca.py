a41026d5532a4a837a6c7d629f501cd3d0bde2d5
JuanJoseBaena <124928411+JuanJoseBaena@users.noreply.github.com>
2023-02-28 08:04:31 -0500

    Create marca.py

---
+++ televisores/marca.py
@@ -0,0 +1,10 @@
+class Marca ():
+    def __innit__(self, nombre):
+        self._nombre = nombre
+
+    def getNombre (self):
+        return self._nombre
+
+    def setNombre (self, nombre):
+        self._nombre = nombre
+        
